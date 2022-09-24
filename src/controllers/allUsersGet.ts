1b646d8907640130ce4bcc3ab18bb1ab49179244
Andrew R <abstractdev0@gmail.com>
2022-09-24 19:21:42 -0400

    feat: Create allUsersGet controller function

---
+++ src/controllers/allUsersGet.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from "express";
+import { verifyCookie } from "../auth/verifyCookie";
+import jwt from "jsonwebtoken";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+export const allUsersGet = [
+  verifyCookie,
+  (req: Request, res: Response) => {
+    jwt.verify(
+      req.cookies.authToken,
+      process.env.JWT_SECRET!,
+      (err: any, authData: any) => {
+        if (err) {
+          res.status(403).json({ 403: "Token Verification Failed" });
+          return;
+        } else if (authData.role === "ADMIN") {
+          (async () => {
+            const users = await prisma.user.findMany({
+              select: {
+                name: true,
+                email: true,
+              },
+            });
+            res.status(200).json(users);
+          })();
+        } else {
+          res.status(403).json({ 403: "Forbidden" });
+        }
+      }
+    );
+  },
+];
